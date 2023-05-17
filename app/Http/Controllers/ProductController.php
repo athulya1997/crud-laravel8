27f1000f1484ced9e3af3387010fe2effd420e39
Athulya <athulyabalakrishnanv@gmail.com>
2023-05-17 14:47:05 +0530

    Retrieve data from the database

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -56,7 +56,7 @@ class ProductController extends Controller
      */
     public function show(Product $product)
     {
-        //
+        return view('products.show',compact('product'));
     }
 
     /**
--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -6,7 +6,7 @@
             <h2>Laravel 8 Crud</h2>
         </div>
         <div class="pull-right">
-            <a class="btn btn-success" href="{{route('products.create')}}"></a>
+            <a class="btn btn-success" href="{{route('products.create')}}">Create New Product</a>
         </div>
     </div>
 </div>
@@ -14,4 +14,30 @@
 <div class="alert alert-success">
     <p>{{$message}}</p>
 </div>
+@endif
+<table class="table table-bordered">
+    <tr>
+        <th>ID</th>
+        <th>Name</th>
+        <th>Detail</th>
+        <th width="280px">Action</th>
+    </tr>
+    @foreach($products as $product)
+    <tr>
+        <td>{{$product->id}}</td>
+        <td>{{$product->name}}</td>
+        <td>{{$product->detail}}</td>
+        <td>
+            <form action="{{route('products.destroy',$product->id)}}" method="POST">
+                <a href="{{route('products.show',$product->id)}}" class="btn btn-info">Show</a>
+                <a href="{{route('products.edit',$product->id)}}" class="btn btn-primary">Edit</a>
+                @csrf
+                @method('DELETE')
+                <button class="btn btn-danger">Delete</button>
+            </form>
+        </td>
+    </tr>
+    @endforeach
+</table>
+
 @endsection
